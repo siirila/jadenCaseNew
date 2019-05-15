9b8534b2fac779a0553486f4d8b2244bd6fb77c0
Jared Siirila <siirila@gmail.com>
2019-05-14 20:13:09 -0400

    Split string logic in to a function

--- jadenCase.js
+++ jadenCase.js
@@ -2,30 +2,6 @@ function jadenCase() {
   // getting the text from the input field, set it equal to variable text
   var text = document.getElementById('original').value;
 
-
-  // Create a new empty string
-  var newText = "";
-
-  // i  t
-  var shouldUpperCase = true;
-  // step through each character of the input string
-  for (var characterIndex = 0; characterIndex < text.length; characterIndex++) {
-    var character = text[characterIndex];
-    // For the first character, uppercase that character and write that to the new string
-    if (character === " ") {
-      newText += character;
-      // for the next character upper case it
-      shouldUpperCase = true;
-    } else if (shouldUpperCase) {
-      newText += character.toUpperCase();
-      shouldUpperCase = false;
-    } else {
-      // otherwise, write the character at that index to the new string
-      newText += character;
-    }
-  }
-
-
       //create a variable newText.  newText is equal to the result of calling the split method on text, and calling the map
       // method on the resulting array
 
@@ -35,6 +11,31 @@ function jadenCase() {
     //turn the array back into a sentence
 
     //insert the newText into the document
-    document.getElementById("editedText").textContent = newText;
+    document.getElementById("editedText").textContent = getJadenCasedVersion(text);
 
+}
+
+function getJadenCasedVersion(text) {
+    // Create a new empty string
+    var newText = "";
+
+    var shouldUpperCase = true;
+    // step through each character of the input string
+    for (var characterIndex = 0; characterIndex < text.length; characterIndex++) {
+      var character = text[characterIndex];
+      // For the first character, uppercase that character and write that to the new string
+      if (character === " ") {
+        newText += character;
+        // for the next character upper case it
+        shouldUpperCase = true;
+      } else if (shouldUpperCase) {
+        newText += character.toUpperCase();
+        shouldUpperCase = false;
+      } else {
+        // otherwise, write the character at that index to the new string
+        newText += character;
+      }
+    }
+
+    return newText;
 }

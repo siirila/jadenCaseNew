d04f50389982811314ad0cb37ed1cf9239b7a733
Jared Siirila <siirila@gmail.com>
2019-05-14 20:36:38 -0400

    Convert to use split and map methods

--- jadenCase.js
+++ jadenCase.js
@@ -2,40 +2,31 @@ function jadenCase() {
   // getting the text from the input field, set it equal to variable text
   var text = document.getElementById('original').value;
 
-      //create a variable newText.  newText is equal to the result of calling the split method on text, and calling the map
-      // method on the resulting array
-
-      //return the first letter of each element in the array (x[0])
-      //concatenate it with the rest of the letters in each element (x.slice(1))
-
-    //turn the array back into a sentence
-
     //insert the newText into the document
     document.getElementById("editedText").textContent = getJadenCasedVersion(text);
+}
 
+function upperCaseWord(word) {
+  // return the first letter of each element in the array (x[0])
+  // concatenate it with the rest of the letters in each element (x.slice(1))
+  var upperCased = word[0].toUpperCase() + word.slice(1);
+
+  return upperCased;
 }
 
 function getJadenCasedVersion(text) {
-    // Create a new empty string
-    var newText = "";
+  // Create a new empty string
+  var newText = "";
 
-    var shouldUpperCase = true;
-    // step through each character of the input string
-    for (var characterIndex = 0; characterIndex < text.length; characterIndex++) {
-      var character = text[characterIndex];
-      // For the first character, uppercase that character and write that to the new string
-      if (character === " ") {
-        newText += character;
-        // for the next character upper case it
-        shouldUpperCase = true;
-      } else if (shouldUpperCase) {
-        newText += character.toUpperCase();
-        shouldUpperCase = false;
-      } else {
-        // otherwise, write the character at that index to the new string
-        newText += character;
-      }
-    }
+  // newText is equal to the result of calling the split method on text, and calling the map
+  // method on the resulting array
+  var words = text.split(" ");
+  console.log(words);
+  var upperCasedWords = words.map(upperCaseWord);
+  console.log(upperCasedWords);
 
-    return newText;
+  // turn the new array back in to a sentence
+  newText = upperCasedWords.join(" ");
+
+  return newText;
 }
